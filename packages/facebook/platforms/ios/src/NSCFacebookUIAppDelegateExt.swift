55b5c5c9d9937d9a2b785e4799e118e7d110d552
william <williamjuan027@gmail.com>
2023-10-16 16:14:33 +0800

    bug: add file path exists before accessing file properties

--- packages/facebook/platforms/ios/src/NSCFacebookUIAppDelegateExt.swift
+++ packages/facebook/platforms/ios/src/NSCFacebookUIAppDelegateExt.swift
@@ -2,7 +2,7 @@ import Foundation
 import UIKit
 import FBSDKCoreKit
 
-
+@objcMembers
 @objc(NSCFacebookUIAppDelegateExt)
 public class NSCFacebookUIAppDelegateExt: NSObject {
 
@@ -16,7 +16,7 @@ public class NSCFacebookUIAppDelegateExt: NSObject {
         NSCFacebookUIAppDelegateExt.observing = true
     }
 
-
+    // Should call openUrl instead of applicationDidFinishLaunchingNotificationEvent
     @objc func applicationDidFinishLaunchingNotificationEvent(_ notification: NSNotification) {
         guard let info = notification.userInfo else {
             ApplicationDelegate.shared.application(
@@ -42,6 +42,28 @@ public class NSCFacebookUIAppDelegateExt: NSObject {
             )
         }
 
+    }
 
+    // TODO: 
+    // we need to add this and make the delegate listen to the applicationOpenURLOptions
+    // event. I've verified that this event gets called with what facebook needs, but
+    // the modal doesn't get closed because ApplicationDelegate.shared.application isn't 
+    // called
+    // I'm 90% sure this is the problem, but I can't test my hypothesis because I can't seem to
+    // trigger this either directly from the app or from here
+    // 
+    // My guess is that this has something to do with how the observers are setup that I don't
+    // quite understand
+    func application(
+        _ app: UIApplication,
+        open url: URL,
+        options: [UIApplication.OpenURLOptionsKey : Any] = [:]
+    ) -> Bool {
+        ApplicationDelegate.shared.application(
+            app,
+            open: url,
+            sourceApplication: options[UIApplication.OpenURLOptionsKey.sourceApplication] as? String,
+            annotation: options[UIApplication.OpenURLOptionsKey.annotation]
+        )
     }
 }
